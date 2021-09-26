a324e4e8fe13e99e846d2e06cacf6a983d165dc2
ybald <ybalderas1005@gmail.com>
2021-09-25 20:43:27 -0400

    Initial commit

---
+++ core/src/ghozti/frc/Game.java
@@ -0,0 +1,30 @@
+package ghozti.frc;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class Game extends com.badlogic.gdx.Game {
+
+	@Override
+	public void create() {
+		setScreen(new GameLauncher());
+	}
+
+	@Override
+	public void render() {
+		super.render();
+	}
+
+	@Override
+	public void dispose() {
+		super.dispose();
+		screen.dispose();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		screen.resize(width, height);
+	}
+}
---
+++ desktop/src/ghozti/frc/desktop/DesktopLauncher.java
@@ -0,0 +1,16 @@
+package ghozti.frc.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ghozti.frc.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+
+		config.title = "frc robot sim";
+		config.width = 1280;
+		config.height = 720;
+	}
+}
