8845e188ec745e91330eee7b1fe406c0109ba03a
ybald <ybalderas1005@gmail.com>
2021-09-26 20:26:52 -0400

    - worked on Timer

--- core/src/ghozti/frc/framework/Timer.java
+++ core/src/ghozti/frc/framework/Timer.java
@@ -1,4 +1,32 @@
 package ghozti.frc.framework;
 
 public class Timer {
+
+    double elapsedTime, endTime;
+    boolean timed;
+
+    public Timer(){
+        timed = false;
+    }
+
+    public Timer(double endTime){
+        timed = true;
+        this.endTime = endTime;
+    }
+
+    public void startTimer(double delta){
+        elapsedTime += delta;
+    }
+
+    public boolean isFinished(){
+        return elapsedTime >= endTime;
+    }
+
+    public double getElapsedTime(){
+        return elapsedTime;
+    }
+
+    public double getABStime(){
+        return Math.abs(elapsedTime);
+    }
 }
