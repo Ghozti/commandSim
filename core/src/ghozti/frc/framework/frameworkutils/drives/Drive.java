b317fd97864f0380d61484885e9f0071538f53f9
ybald <ybalderas1005@gmail.com>
2021-09-29 17:52:43 -0400

    - worked on Drive.java Constructor

--- core/src/ghozti/frc/framework/frameworkutils/drives/Drive.java
+++ core/src/ghozti/frc/framework/frameworkutils/drives/Drive.java
@@ -1,4 +1,10 @@
 package ghozti.frc.framework.frameworkutils.drives;
 
+import ghozti.frc.framework.frameworkutils.hardware.SpeedController;
+
 public class Drive {
+
+    public Drive(SpeedController speedController1, SpeedController speedController2){
+
+    }
 }
