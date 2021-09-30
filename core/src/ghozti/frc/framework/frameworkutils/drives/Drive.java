b95384aee24cf3199575e660bd4aa53ffccaa040
ybald <ybalderas1005@gmail.com>
2021-09-29 21:49:11 -0400

    - worked on Drive.java

--- core/src/ghozti/frc/framework/frameworkutils/drives/Drive.java
+++ core/src/ghozti/frc/framework/frameworkutils/drives/Drive.java
@@ -4,7 +4,23 @@ import ghozti.frc.framework.frameworkutils.hardware.SpeedController;
 
 public class Drive {
 
+    SpeedController speedController1, speedController2;
+
     public Drive(SpeedController speedController1, SpeedController speedController2){
+        this.speedController1 = speedController1;
+        this.speedController2 = speedController2;
+    }
+
+    public void arcadeDrive(double speed1, double speed2){
+        setLeftSpeed(speed1);
+        setRightSpeed(speed2);
+    }
+
+    public void setLeftSpeed(double speed){
+
+    }
+
+    public void setRightSpeed(double speed){
 
     }
 }
