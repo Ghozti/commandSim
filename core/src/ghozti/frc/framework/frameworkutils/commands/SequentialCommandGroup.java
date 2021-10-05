f17157bbd7f9717a08694c789bda2c14b6666bcc
Ghozti <ybalderas1005@gmail.com>
2021-10-05 10:54:58 -0400

    - worked on SequentialCommandGroup

--- core/src/ghozti/frc/framework/frameworkutils/commands/SequentialCommandGroup.java
+++ core/src/ghozti/frc/framework/frameworkutils/commands/SequentialCommandGroup.java
@@ -1,16 +1,32 @@
 package ghozti.frc.framework.frameworkutils.commands;
 
-import ghozti.frc.framework.frameworkutils.commands.Command;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class SequentialCommandGroup {
 
-    void addCommands(Command... commands){
-        for(Command c : commands){
-            c.init();
-            if (!c.isFinished()){
-                c.execute();
-            }else {
-                c.end();
+    ArrayList<Command> command_lst = new ArrayList<>();
+
+    protected void addCommands(Command... commands){
+        command_lst.addAll(Arrays.asList(commands));
+    }
+
+    public void executeCommands(){
+        boolean newCommandInit = true;
+        for(int i = 0; i < command_lst.size(); i+=0){
+
+            if (newCommandInit){
+                command_lst.get(i).init();
+                newCommandInit = false;
+            }
+
+            if (!command_lst.get(i).isFinished()){
+                command_lst.get(i).execute();
+                executeCommands();
+            }else{
+                command_lst.get(i).end();
+                newCommandInit = true;
+                i++;
             }
         }
     }
