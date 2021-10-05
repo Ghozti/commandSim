7487211cc66fdbeed6e4343c18f2f35c6ff6f44f
Ghozti <ybalderas1005@gmail.com>
2021-10-05 11:19:11 -0400

    - worked on SequentialCommandGroup

--- core/src/ghozti/frc/framework/frameworkutils/commands/SequentialCommandGroup.java
+++ core/src/ghozti/frc/framework/frameworkutils/commands/SequentialCommandGroup.java
@@ -5,29 +5,36 @@ import java.util.Arrays;
 
 public class SequentialCommandGroup {
 
-    ArrayList<Command> command_lst = new ArrayList<>();
-
     protected void addCommands(Command... commands){
         command_lst.addAll(Arrays.asList(commands));
     }
 
+    ArrayList<Command> command_lst = new ArrayList<>();
+    boolean newCommandInit = true;
+    int currentInd = 0;
+
     public void executeCommands(){
-        boolean newCommandInit = true;
-        for(int i = 0; i < command_lst.size(); i+=0){
 
-            if (newCommandInit){
-                command_lst.get(i).init();
-                newCommandInit = false;
-            }
-
-            if (!command_lst.get(i).isFinished()){
-                command_lst.get(i).execute();
-                executeCommands();
-            }else{
-                command_lst.get(i).end();
-                newCommandInit = true;
-                i++;
-            }
+        if (newCommandInit){
+            command_lst.get(currentInd).init();
+            newCommandInit = false;
         }
+
+        if (!command_lst.get(currentInd).isFinished()){
+            command_lst.get(currentInd).execute();
+            executeCommands();
+        }else{
+            command_lst.get(currentInd).end();
+            newCommandInit = true;
+            currentInd++;
+        }
+    }
+
+    public int getListSize(){
+        return command_lst.size();
+    }
+
+    public int getCurrentIndex(){
+        return currentInd;
     }
 }
