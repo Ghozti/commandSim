36c67f79c306e44d1cfa74c721502d84ee053e3d
Ghozti <ybalderas1005@gmail.com>
2021-10-04 21:38:20 -0400

    - worked on SequentialCommandGroup

--- core/src/ghozti/frc/framework/frameworkutils/commands/SequentialCommandGroup.java
+++ core/src/ghozti/frc/framework/frameworkutils/commands/SequentialCommandGroup.java
@@ -6,7 +6,12 @@ public class SequentialCommandGroup {
 
     void addCommands(Command... commands){
         for(Command c : commands){
-
+            c.init();
+            if (!c.isFinished()){
+                c.execute();
+            }else {
+                c.end();
+            }
         }
     }
 }
