6c41468bfaff9f506014f55250a90d0cdee7ed86
Ghozti <ybalderas1005@gmail.com>
2021-10-06 10:40:45 -0400

    - worked on Constants

--- core/src/ghozti/frc/robot/utils/Constants.java
+++ core/src/ghozti/frc/robot/utils/Constants.java
@@ -1,18 +1,24 @@
 package ghozti.frc.robot.utils;
 
 import com.badlogic.gdx.graphics.Texture;
+import org.w3c.dom.Text;
 
 public class Constants {
 
-    public static class keys{
-        public static String UP = "W";
-        public static String DOWN = "W";
-        public static String LEFT = "W";
-        public static String RIGHT = "W";
+    public static class Game{
+        public static final float width = 1280;// screen width
+        public static final float height = 720;//screen height
     }
 
-    public static class Robot{
-        public static Texture TEXTURE;// TODO fill this
-        public static float maxSpeed = 1;
+    public static class Robot {
+        public static final Texture robotTexture = null;//TODO fill this
+        public static final double maxRobotSpeed = 15;// in pixels pixels
+        public static final double acceleration = 5;//in pixels
+    }
+
+    public static class Field{
+        public static final Texture fieldTexture = null;//TODO fill this
+        public static final double width = 1280;//field width
+        public static final double height = 720;//field height
     }
 }
