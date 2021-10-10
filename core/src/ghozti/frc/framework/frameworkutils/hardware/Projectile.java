ac23ef8032801c37180045ce994674443c1a7364
Ghozti <ybalderas1005@gmail.com>
2021-10-10 19:41:35 -0400

    - worked on Projectile.java

--- core/src/ghozti/frc/framework/frameworkutils/hardware/Projectile.java
+++ core/src/ghozti/frc/framework/frameworkutils/hardware/Projectile.java
@@ -1,4 +1,33 @@
 package ghozti.frc.framework.frameworkutils.hardware;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.math.Rectangle;
+
 public class Projectile {
+
+    float width, height;
+    Texture projectileTexture;
+    float[] position;
+    Rectangle hitbox;
+
+    public Projectile(float initx, float inity){
+        projectileTexture = new Texture("ball.png");
+        position[0] = initx;
+        position[1] = inity;
+        width = 50;
+        height = 50;
+        hitbox = new Rectangle(position[0],position[1],width,height);
+    }
+
+    public void update(float xchange, float ychange){
+        position[0] += xchange;
+        position[1] += ychange;
+        hitbox.x = position[0];
+        hitbox.y = position[1];
+    }
+
+    public void draw(Batch batch){
+        batch.draw(projectileTexture, position[0],position[1],width,height);
+    }
 }
--- core/src/ghozti/frc/robot/subsystems/Shooter.java
+++ core/src/ghozti/frc/robot/subsystems/Shooter.java
@@ -1,12 +1,14 @@
 package ghozti.frc.robot.subsystems;
 
 import com.badlogic.gdx.math.Rectangle;
+import ghozti.frc.framework.frameworkutils.hardware.Projectile;
 
 public class Shooter {
 
     boolean hasFired;
     float[] position;
     Rectangle hitbox;
+    Projectile projectile;
 
     public Shooter(){
 
