36e5092ceb9fddacbd5e20b7e262265abb915665
Ghozti <ybalderas1005@gmail.com>
2021-10-10 20:41:20 -0400

    - worked on Shooter

--- core/src/ghozti/frc/robot/subsystems/Shooter.java
+++ core/src/ghozti/frc/robot/subsystems/Shooter.java
@@ -7,10 +7,25 @@ public class Shooter {
 
     boolean hasFired;
     float[] position;
+    float width, height;
     Rectangle hitbox;
     Projectile projectile;
 
-    public Shooter(){
+    public Shooter(float posx, float posy){
+        hasFired = false;
+        position[0] = posx;
+        position[1] = posy;
+        width = 50;
+        height = 50;
+        hitbox = new Rectangle(position[0],position[1],width,height);
+        projectile = new Projectile(position[0],position[1]);
+    }
 
+    public void updateShooter(){
+
+    }
+
+    public void shoot(){
+        
     }
 }
